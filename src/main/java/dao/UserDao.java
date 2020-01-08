c18575fa46eab59bf9da2d04cf36f9d9be2e0955
cyj0619 <cyj0619@midasit.com>
2020-01-09 08:44:26 +0900

    [3장 템플릿] JdbcTemplate 템플릿/콜백 적용
      - develop : 1. 스프링에서 제공하는 템플릿/콜백 중 JdbcTemplate 적용

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,97 +1,93 @@
 package dao;
 
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import vo.User;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class UserDao {
-    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
-    public void setDataSource(DataSource dataSource) {
-        if (this.dataSource == null) {
-            this.dataSource = dataSource;
-        }
-    }
-
-    public void add(User user) throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement(
-                "insert into users(id, name, password) values(?, ?, ?)"
-        );
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
-    }
-
-    public User get(String id) throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement(
-                "select * from users where id = ?"
-        );
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-
-        User user = null;
-
-        if (rs.next()) {
-            user = new User();
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        public User mapRow(ResultSet rs, int i) throws SQLException {
+            User user = new User();
             user.setId(rs.getString("id"));
             user.setName(rs.getString("name"));
             user.setPassword(rs.getString("password"));
+            return user;
         }
+    };
 
-        rs.close();
-        ps.close();
-        c.close();
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
-        if (user == null) {
-            throw new EmptyResultDataAccessException(1);
-        }
+    public void add(User user) throws SQLException {
+        this.jdbcTemplate.update(
+                "insert into users(id, name, password) values(?, ?, ?)",
+                user.getId(),
+                user.getName(),
+                user.getPassword()
+        );
+    }
 
-        return user;
+    public User get(String id) throws SQLException {
+        return this.jdbcTemplate.queryForObject(
+                "select * from users where id = ?",
+                new Object[]{id},
+                this.userMapper
+        );
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement(
-                "delete from users"
-        );
-
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+        this.jdbcTemplate.update("delete from users");
     }
 
     public int getCount() throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement(
-                "select count(*) from users"
-        );
-
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        return count;
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
+
+    public List<User> getAll() {
+        return this.jdbcTemplate.query(
+                "select * from users order by id asc",
+                this.userMapper
+        );
+    }
+
+    /**
+     * 전략패턴 중 Context에 해당하는 부분(변하지 않는 부분)
+     *   - JdbcTemplate 사용으로 인해 더이상 사용하지 않는다.
+     * */
+    /*public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    // empty
+                }
+            }
+
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                    // empty
+                }
+            }
+        }
+    }*/
 }
--- src/test/java/daoTest/UserDaoTest.java
+++ src/test/java/daoTest/UserDaoTest.java
@@ -5,16 +5,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import vo.User;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -43,7 +42,6 @@ import static org.junit.Assert.assertThat;
 @ContextConfiguration(locations = "/test-applicationContext.xml")
 //@DirtiesContext
 public class UserDaoTest {
-
     /**
      * @Autowired
      *  - 스프링 DI에 사용되는 어노테이션
@@ -128,5 +126,37 @@ public class UserDaoTest {
         assertThat(dao.getCount(), is(0));
 
         dao.get("unknown_ID");
+    }
+
+    @Test
+    public void getAll() throws SQLException {
+        dao.deleteAll();
+
+        List<User> users0 = dao.getAll();
+        assertThat(users0.size(), is(0));
+
+        dao.add(user1);
+        List<User> users1 = dao.getAll();
+        assertThat(users1.size(), is(1));
+        checkSameUser(user1, users1.get(0));
+
+        dao.add(user2);
+        List<User> users2 = dao.getAll();
+        assertThat(users2.size(), is(2));
+        checkSameUser(user1, users2.get(0));
+        checkSameUser(user2, users2.get(1));
+
+        dao.add(user3);
+        List<User> users3 = dao.getAll();
+        assertThat(users3.size(), is(3));
+        checkSameUser(user1, users3.get(0));
+        checkSameUser(user2, users3.get(1));
+        checkSameUser(user3, users3.get(2));
+    }
+
+    private void checkSameUser(User user1, User user2) {
+        assertThat(user1.getId(), is(user2.getId()));
+        assertThat(user1.getName(), is(user2.getName()));
+        assertThat(user1.getPassword(), is(user2.getPassword()));
     }
 }
